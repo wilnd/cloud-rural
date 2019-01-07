aace60aec508a760821f3839aa2fbcfd3e638559
wilnd <287329409@qq.com>
2019-01-07 22:29:41 +0800

    init

---
+++ rural/gateway-zuul/src/main/java/spring_cloud_rural/App.java
@@ -0,0 +1,13 @@
+package spring_cloud_rural;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
